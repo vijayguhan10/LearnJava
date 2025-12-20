bbf0bc321104dcab99348f6655c995252eddabf3
vijayguhan10 <vijayguhan10@gmail.com>
2025-12-20 12:32:40 +0530

    updated with the helper clas

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
-class Main{
+class Helper {
+    public static void greet() {
+        System.out.println("Greetings from helper class!");
+    }
+}
 
-
-public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  
-
-
-}}
+public class Main {
+    public static void main(String[] args) {
+        Helper.greet();
+        System.out.println("Hello, World!");
+    }
+}
