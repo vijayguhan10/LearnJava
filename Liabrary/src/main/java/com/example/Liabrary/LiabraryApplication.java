dee74a71a3db320d6806521ac03dfede0b3c7c1e
vijayguhan10 <vijayguhan10@gmail.com>
2026-01-14 20:59:37 +0530

    Updated with the Application of Server Code

--- Liabrary/src/main/java/com/example/Liabrary/LiabraryApplication.java
+++ Liabrary/src/main/java/com/example/Liabrary/LiabraryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LiabraryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Liabrary Application Started");
 		SpringApplication.run(LiabraryApplication.class, args);
 	}
 
