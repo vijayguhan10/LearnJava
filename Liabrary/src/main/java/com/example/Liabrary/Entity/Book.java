93e6b915a63d925cbe2cff4d71379cbafa9b1afe
vijayguhan10 <vijayguhan10@gmail.com>
2026-01-16 20:34:30 +0530

    Updated with the application POM XML

--- Liabrary/src/main/java/com/example/Liabrary/Entity/Book.java
+++ Liabrary/src/main/java/com/example/Liabrary/Entity/Book.java
@@ -12,7 +12,7 @@ import jakarta.persistence.OneToOne;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     private String title;
