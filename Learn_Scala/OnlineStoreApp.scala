1db7e1f8f480d056c9a6507f00b75d4a79ceeaec
vijayguhan10 <vijayguhan10@gmail.com>
2026-01-20 23:15:20 +0530

    updated the Application

---
+++ Learn_Scala/OnlineStoreApp.scala
@@ -0,0 +1,57 @@
+// Interface (Trait) - Abstraction
+trait PaymentMethod {
+  def pay(amount: Double): Unit
+}
+
+// Abstract Class - Partial Implementation
+abstract class Payment(val methodName: String) extends PaymentMethod {
+
+  // Encapsulation (protected data)
+  protected var totalPaid: Double = 0
+
+  // Concrete method
+  def printReceipt(amount: Double): Unit = {
+    totalPaid += amount
+    println(s"$methodName payment of $$amount successful")
+  }
+}
+
+// Concrete Class - Inheritance + Override
+class UpiPayment extends Payment("UPI") {
+  override def pay(amount: Double): Unit = {
+    printReceipt(amount)
+  }
+}
+
+// Concrete Class - Inheritance + Override
+class CardPayment extends Payment("Card") {
+  override def pay(amount: Double): Unit = {
+    printReceipt(amount)
+  }
+}
+
+// Business Logic Class - Encapsulation
+class Order(private val orderId: Int, private val amount: Double) {
+
+  // Polymorphism: depends on interface
+  def processPayment(payment: PaymentMethod): Unit = {
+    println(s"Processing order $orderId")
+    payment.pay(amount)
+  }
+}
+
+// Singleton Object - Program Entry Point
+object OnlineStoreApp {
+
+  def main(args: Array[String]): Unit = {
+
+    val order = new Order(101, 500.0)
+
+    val upi: PaymentMethod = new UpiPayment()
+    val card: PaymentMethod = new CardPayment()
+
+    order.processPayment(upi)
+    order.processPayment(card)
+  }
+}
+
