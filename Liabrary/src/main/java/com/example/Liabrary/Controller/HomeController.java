363070693626adb2139588ebcf810e1d69e92239
vijayguhan10 <vijayguhan10@gmail.com>
2026-01-24 12:35:39 +0530

    Added the Home Application

---
+++ Liabrary/src/main/java/com/example/Liabrary/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.Liabrary.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1 style={{\"color\":\"green\"}}>Application Started Successfully! </h1>" +
+                "<p>Welcome to the Library Management System</p>" +
+                "<p>API Endpoints available at <a href='/books'>/books</a></p>";
+    }
+}
