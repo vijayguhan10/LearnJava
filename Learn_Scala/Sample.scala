0437a2bb4df944564122fac3b8bd18cb93decbf3
vijayguhan10 <vijayguhan10@gmail.com>
2026-04-07 09:12:06 +0530

    Working with the pattern matching and with the unapply

--- Learn_Scala/Sample.scala
+++ Learn_Scala/Sample.scala
@@ -6,4 +6,20 @@ object Sample extends App {
 
   println(u1)
   println(u2)
+//   def greet(user: User): String = {
+
+//   User.unapply(user) match {
+
+//     case Some(("Vijay", age)) =>
+//       s"Admin Vijay, age $age"
+
+//     case Some((name, age)) =>
+//       s"Hello $name, age $age"
+//   }
+// }
+  def greet(emp:User):String=emp match{
+    case User("Vijay",age)=>s"Vijay Is matched properly"
+    case User(name,age)=>s"$name is printing"
+  }
+  println(greet(u1))
 }
